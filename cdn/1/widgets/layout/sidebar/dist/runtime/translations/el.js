979efd1e8f78710508f6bb8aad471328728a3ca6
djuracek2 <djuracek@blm.gov>
2024-09-29 10:33:06 -0600

    new deployment

--- cdn/1/widgets/layout/sidebar/dist/runtime/translations/el.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(e){return{execute:function(){e({_widgetLabel:"Πλαϊνή γραμμή",_layout_FIRST_label:"Πρώτο",_layout_SECOND_label:"Δευτερολέπτων",_action_toggleSidebar_label:"Εναλλαγή πλαϊνής μπάρας",widgetProperties:"Ιδιότητες του widget",widgetFunctions:"Λειτουργίες του widget",widgetName:"όνομα του widget:",widgetProps:"ιδιότητες του widget:"})}}}));
